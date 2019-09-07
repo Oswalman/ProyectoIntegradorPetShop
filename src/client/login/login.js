a709662c3ce2f4571e8ea4d6f0e44033cbcdcfbd
Andres Felipe Herrera Jerez <33936503+Andresxv@users.noreply.github.com>
2019-09-07 13:44:33 -0500

    login preview x3

--- src/client/login/login.js
+++ src/client/login/login.js
@@ -1,5 +1,5 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
+const signUpButton = document.getElementById('registro');
+const signInButton = document.getElementById('inicio');
 const container = document.getElementById('container');
 
 signUpButton.addEventListener('click', () => {
