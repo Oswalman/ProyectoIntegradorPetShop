a5dfc65f40ba12f0ed96d7ea805a320232baaf0f
PapiCarryShongo <jeca010198@gmail.com>
2019-09-29 21:39:15 -0500

    fotos de perfil

--- frontend/src/client/views/About/index.js
+++ frontend/src/client/views/About/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import '../../style/perfiles.css';
-//import foto1 from '../img/je.png';
-//import foto2 from '../img/oa.png';
-//import foto3 from '../img/fa.png';
-//import foto4 from '../img/af.png';
+import foto1 from '../../img/je.png';
+import foto2 from '../../img/oa.png';
+import foto3 from '../../img/fa.png';
+import foto4 from '../../img/af.png';
 
 
 
@@ -14,28 +14,28 @@ class About extends React.Component
           <div className="row containerP">
             <div className="col-lg-3 pf">
               <div className="img">
-                <img></img>
+                <img src={foto1} className="img-P"></img>
               </div>
               <h3>Jhon Edinson</h3>
               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus officia aliquam iure vitae et. Animi nisi illo iste numquam repellendus!</p>
             </div>
             <div className="col-lg-3 pf">
               <div className="img">
-                <img></img>
+                <img src={foto2} className="img-P" ></img>
               </div>
               <h3>Oswaldo Andres</h3>
               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus officia aliquam iure vitae et. Animi nisi illo iste numquam repellendus!</p>
             </div>
             <div className="col-lg-3 pf">
               <div className="img">
-                <img></img>
+                <img src={foto3} className="img-P"></img>
               </div>
               <h3>Fabian Andres</h3>
               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus officia aliquam iure vitae et. Animi nisi illo iste numquam repellendus!</p>
             </div>
             <div className="col-lg-3 pf">
               <div className="img">
-                <img></img>
+                <img src={foto4} className="img-P"></img>
               </div>
               <h3>Andres Felipe</h3>
               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus officia aliquam iure vitae et. Animi nisi illo iste numquam repellendus!</p>
