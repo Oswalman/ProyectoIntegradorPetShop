c91521747b31be1c0860e2d10a9c176303f9c404
PapiCarryFabs <fabiandresbarreto@gmail.com>
2019-10-05 12:58:31 -0500

    Import SliderPredefinido
    Que no se como usar xd

--- frontend/src/client/views/home/index.js
+++ frontend/src/client/views/home/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Slider from '../../components/slider';
-//import '../../style/style.css';
+import AwesomeSlider from 'react-awesome-slider';
+import 'react-awesome-slider/dist/styles.css'
 
+//import '../../style/style.css';
+const slider = (
+  <AwesomeSlider>
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+    <div>4</div>
+  </AwesomeSlider>
+);
 const Home = () => {
   return (<Slider></Slider>);
 };
