5ecfa0d9400a19a3f6e9b6fcd2e069d5e12acc85
PapiCarryShongo <jeca010198@gmail.com>
2019-10-17 23:43:59 -0500

    ya funco :3 soy feliz xD

--- frontend/src/client/views/detailsMP/index.js
+++ frontend/src/client/views/detailsMP/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import DetallesMP from '../../components/detailsPet';
 
+
+
 class DetallesPet extends React.Component {
     state = {
-        detalles: localStorage.getItem('lostPet')
+        detalles:JSON.parse(localStorage.getItem('lostPet'))
     }
+    
     render(){
-        return(
-            <div className="container-fluid">
-                <p>{this.state.detalles}</p>
+        return(            
+            <div className="container-build" style={{background:"white", top:"0px"}}>
+                <DetallesMP send={this.state.detalles}></DetallesMP>
             </div>
         );
     }
